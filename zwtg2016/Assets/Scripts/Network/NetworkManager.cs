56de5362f6b427baf0a16bb28c0a33fd6ad25ef8
Kacper Sokołowski <kacper.sokolowski@codewise.com>
2016-05-08 13:49:52 +0200

    Integrated photon plugin

---
+++ zwtg2016/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	const string VERSION = "0.1";
+	const string ROOM_NAME = "lobby";
+
+	public GameObject spawnPoint;
+	public GameObject prefab;
+
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings(VERSION);
+	}
+
+
+	void OnJoinedLobby() {
+		RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+
+		// NOT THREAD SAFE!!
+		PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, roomOptions, TypedLobby.Default);
+	}
+
+	void OnJoinedRoom() {
+		PhotonNetwork.Instantiate(prefab.name, spawnPoint.transform.position, spawnPoint.transform.rotation, 0);
+	}
+}

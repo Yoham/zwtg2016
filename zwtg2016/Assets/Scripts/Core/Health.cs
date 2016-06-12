42e65328f4e245981ec2a9c17847aacf4bb5cc65
Mateusz <makaczma17@gmail.com>
2016-06-12 20:35:42 +0200

    BetaShooting Pew Pew

---
+++ zwtg2016/Assets/Scripts/Core/Health.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+    public float hitPoints = 100f;
+    float currentHitPoints;
+    // Use this for initialization
+    void Start()
+    {
+        currentHitPoints = hitPoints;
+    }
+
+    [PunRPC]
+    public void TakeDamage(float dmg)
+    {
+        currentHitPoints -= dmg;
+
+        if (currentHitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (GetComponent<PhotonView>().instantiationId == 0)
+        {
+            Destroy(gameObject);
+        }
+        else {
+            if (PhotonNetwork.isMasterClient)
+            {
+                PhotonNetwork.Destroy(gameObject);
+            }
+        }
+    }
+
+}
+
---
+++ zwtg2016/Assets/Scripts/Core/Shooting.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+
+    public float fireRate = 0.5f;
+    float cooldown = 0;
+    public float damage = 25f;
+    FX_Manager m_FX_Manager;
+
+    void Start()
+    {
+        m_FX_Manager = GameObject.FindObjectOfType<FX_Manager>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        if (cooldown > 0)
+        {
+            return;
+        }
+        Debug.Log("Shoting!!");
+
+        Vector3 hitPoint;
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Transform hitTransform = findClosestHitObject(ray, out hitPoint);
+
+        if (hitTransform != null)
+        {
+
+            Debug.Log("We hit: " + hitTransform.name);
+
+            Health h = hitTransform.GetComponent<Health>();
+
+            while (h == null && hitTransform.parent)
+            {
+                hitTransform = hitTransform.parent;
+                h = hitTransform.GetComponent<Health>();
+            }
+
+            if (h != null)
+            {
+                h.GetComponent<PhotonView>().RPC("TakeDamage", PhotonTargets.AllBuffered, damage);
+            }
+
+
+            if (m_FX_Manager != null)
+            {
+                m_FX_Manager.GetComponent<PhotonView>().RPC("SniperBulletFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
+            }
+        }
+        else
+        {
+            if (m_FX_Manager != null)
+            {
+                hitPoint = Camera.main.transform.position + (Camera.main.transform.forward * 100f);
+                m_FX_Manager.GetComponent<PhotonView>().RPC("SniperBulletFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
+            }
+        }
+
+
+        cooldown = fireRate;
+    }
+
+    Transform findClosestHitObject(Ray ray, out Vector3 hitPoint)
+    {
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        Transform closestHit = null;
+        hitPoint = Vector3.zero;
+        float distance = 0;
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.transform != this.transform && (closestHit == null || hit.distance < distance))
+            {
+                closestHit = hit.transform;
+                distance = hit.distance;
+                hitPoint = hit.point;
+            }
+        }
+        return closestHit;
+    }
+}

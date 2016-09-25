48697b26a8a9c6e6d4926caba632b04ea35e5723
Adrian Czapla <kuzapura@gmail.com>
2016-09-25 08:06:39 +0200

    Added crosshair (Crosshair.cs, crosshair.png) for FPSController.

---
+++ zwtg2016/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+    Rect crosshairRectangle;
+    Texture2D crosshairTexture;
+
+	// Use this for initialization
+	void Start () {
+        crosshairTexture = Resources.Load ("Textures/crosshair") as Texture2D;
+        crosshairRectangle = new Rect (Screen.width  / 2 - crosshairTexture.width  / 2,
+                                       Screen.height / 2 - crosshairTexture.height / 2,
+                                       crosshairTexture.width, crosshairTexture.height);
+	}
+
+    // Draws once per frame, like Update() does
+    void OnGUI () {
+        GUI.DrawTexture (crosshairRectangle, crosshairTexture);
+    }
+}
